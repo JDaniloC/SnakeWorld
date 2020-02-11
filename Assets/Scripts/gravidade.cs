4797c0d01b2c1ee836d510466e1f386c81a0ba48
JDaniloC <jdsc@cin.ufpe.br>
2020-02-11 13:12:14 -0300

    Criação do Cenário e movimentação

---
+++ Assets/Scripts/gravidade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravidade : MonoBehaviour {
+
+    public PlanetScript planeta;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+        playerTransform = transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (planeta)
+        {
+            planeta.Attract(playerTransform);
+        }
+    }
+}
---
+++ Assets/Scripts/movimentarJogador.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentarJogador : MonoBehaviour {
+    public float velocidadeDeMovimento;
+    private Vector3 direcao;
+
+    void Update()
+    {
+        // Pega a direção a cada update
+        direcao = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Se movimenta
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(direcao) * velocidadeDeMovimento * Time.deltaTime);
+    }
+}
